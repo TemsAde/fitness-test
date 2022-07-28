c3d53a2ecd74798b565197b82a931f30dce797fa
Temi Adeoti <temmyadeoti312@gmail.com>
2022-07-28 17:47:06 -0400

    html added

---
+++ script.js
@@ -0,0 +1,54 @@
+const reset = document.getElementById("Bey");
+
+reset.addEventListener("click", (event) => {
+    event.target.style.background = "brown";
+    event.target.style.color = "white"
+})
+
+const reset2 = document.getElementById("ade");
+reset2.addEventListener("click", (event) => {
+    event.target.style.background = "black";
+    event.target.style.color = "blue"
+})
+
+const reset3 = document.getElementById("tem");
+reset3.addEventListener("click", (event) => {
+    event.target.style.background = "grey";
+    event.target.style.color = "red"
+})
+
+const reset4 = document.getElementById("tay");
+reset4.addEventListener("click", (event) => {
+    event.target.style.background = "pink";
+    event.target.style.color = "yellow"
+})
+
+const reset5 = document.getElementById("keh");
+reset5.addEventListener("click", (event) => {
+    event.target.style.background = "violet";
+    event.target.style.color = "brown"
+})
+
+const reset6 = document.getElementById("nic");
+reset6.addEventListener("click", (event) => {
+    event.target.style.background = "orange";
+    event.target.style.color = "green"
+})
+
+const reset7 = document.getElementById("sim");
+reset7.addEventListener("click", (event) => {
+    event.target.style.background = "red";
+    event.target.style.color = "blue"
+})
+
+const reset8 = document.getElementById("jul");
+reset8.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "white"
+})
+
+const reset9 = document.getElementById("oli");
+reset9.addEventListener("click", (event) => {
+    event.target.style.background = "yellow";
+    event.target.style.color = "orange"
+})
